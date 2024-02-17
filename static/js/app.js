3d2c3b4c10241f6728a510c924e1b2932eb32cb7
Qwertykitten <146257477+MaisieDeGraaf@users.noreply.github.com>
2024-02-17 18:04:22 -0500

    Bubble Graph

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,5 @@
 let samples = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-
 function init() {
     d3.json(samples).then(data => {
         console.log(data); 
@@ -10,32 +9,41 @@ function init() {
             dropdown.append("option").text(name).property("value", name);
         });
         optionChanged(data.names[0]);
-    })
+    });
 }
-
 
 function optionChanged(selectedId) {
     d3.json(samples).then(data => {
         console.log(data); 
 
         let sample = data.samples.find(sample => sample.id === selectedId);
-        let SampleValues = sample.sample_values.slice(0, 10).reverse();
-        let OtuIds = sample.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse();
-        let OtuLabels = sample.otu_labels.slice(0, 10).reverse();
+        let sampleValues = sample.sample_values.slice(0, 10).reverse();
+        let otuIds = sample.otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse();
+        let otuLabels = sample.otu_labels.slice(0, 10).reverse();
 
-        let BarData = {
-            x: SampleValues,
-            y: OtuIds, 
-            text: OtuLabels,
+        let barData = {
+            x: sampleValues,
+            y: otuIds, 
+            text: otuLabels,
             type: "bar",
             orientation: "h"
         };
 
+        let bubbleData = {
+            x: sample.otu_ids,
+            y: sample.sample_values,
+            mode: 'markers',
+            marker: {
+                size: sample.sample_values,
+                color: sample.otu_ids,
+                colorscale: 'Earth'
+            },
+            text: sample.otu_labels
+        };
 
-        let plotData = [BarData];
-
-        Plotly.newPlot("bar", plotData);
-    })
+        Plotly.newPlot('bar', [barData]);
+        Plotly.newPlot('bubble', [bubbleData], { xaxis: { title: 'OTU ID' } });
+    });
 }
 
 init();
